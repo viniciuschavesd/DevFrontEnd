a1a20ba332d7bdd676384b434b59a954bd02b782
viniciuschavesd <vini.chaves@live.com>
2026-09-01 19:45:36 -0300

    Aula 03

--- aula03/Exemplo01.js
+++ aula03/Exemplo01.js
@@ -1,1 +1,14 @@
-a
+function aumentaSalario(j){
+    j.salario=1500000;
+}
+
+let jogador = {
+    nome: "Garrincha",
+    posicao: "atacante",
+    salario: 12000 ,
+    altura: 10
+};
+
+aumentaSalario(jogador);
+
+console.log('Salário: ', jogador.salario);
