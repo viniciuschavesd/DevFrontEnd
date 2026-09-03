3f695070b7c924050531c458a17c8ffa9c08aa2b
viniciuschavesd <vini.chaves@live.com>
2026-09-02 21:47:30 -0300

    Update

--- aula04/Exemplo02.js
+++ aula04/Exemplo02.js
@@ -7,11 +7,12 @@ let usuario = {
 };
 
 for (let nomePropriedade in usuario) {
-    console.log(usuario[nomePropriedade]);
+    console.log(usuario[nomePropriedade]); //Usar somente nomePropriedade irá imprimir as propriedades
 }
 
 console.log("\n-Vetor:");
 let vetor = ["Batman", "Flash", "Thanos", "Visão"];
+
 for (let nomeVetor in vetor){
-    console.log(vetor[nomeVetor]);
+    console.log(vetor[nomeVetor]); //Usar somente nomeVetor irá imprimir os indices
 }
---
+++ aula04/Exemplo03.js
@@ -0,0 +1,6 @@
+console.log("For of: ");
+console.log("-Vetor: ");
+let herois = ['Batman', 'Wonder Woman',    'Flash'];
+for (let valor of herois) {
+    console.log(valor);
+}
