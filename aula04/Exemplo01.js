bcb798269a131edb4adc2d44665ac3c89a4a8907
viniciuschavesd <vini.chaves@live.com>
2026-09-02 21:02:09 -0300

    Update

---
+++ aula04/Exemplo01.js
@@ -0,0 +1,13 @@
+const somar = function(a,b) {
+    return a + b;
+}
+
+//Arrow function
+const somar2 = (a, b) => a + b;
+    
+
+let a = 2;
+let b = 5;
+
+console.log("Function" + somar(a, b));
+console.log("Arrow function" + somar2(a, b));
